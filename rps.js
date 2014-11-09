33582a09736c3670fcc4168a76c745ca33449ad1
Robin Murphy <robin.murphy@bbc.co.uk>
2014-11-09 16:28:49 +0000

    Refactor period into variable

--- rps.js
+++ rps.js
@@ -15,13 +15,14 @@ AWS.config.update({
 
 var cloudWatch = new AWS.CloudWatch();
 var now = Date.now() / 1000;
+var period = 60;
 var options = {
   Namespace: "AWS/ELB",
   MetricName: "RequestCount",
   Statistics: ["Sum"],
-  StartTime: now - 60,
+  StartTime: now - period,
   EndTime: now,
-  Period: 60,
+  Period: period,
   Dimensions: [{
     Name: "LoadBalancerName",
     Value: loadBalancerName
@@ -32,7 +33,7 @@ cloudWatch.getMetricStatistics(options, function (err, data) {
   if (err) return console.error(err.message);
 
   if (data.Datapoints && data.Datapoints.length > 0) {
-    console.log((data.Datapoints[0].Sum / 60).toFixed(0));
+    console.log((data.Datapoints[0].Sum / period).toFixed(0));
   } else {
     console.error('No data found for ELB', loadBalancerName, 'in region', region);
   }
